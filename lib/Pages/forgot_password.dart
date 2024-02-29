1b0cf6dac93aa490908fda07cc50f72e772ed6f9
dhanashrishisode12 <dhanashrishisode17@gmail.com>
2024-02-29 16:00:35 +0530

    completed

--- lib/Pages/forgot_password.dart
+++ lib/Pages/forgot_password.dart
@@ -1,6 +1,12 @@
+
+
+
+
+
+
 import 'package:flutter/material.dart';
 import 'package:interenshala_assignment/Pages/login.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:interenshala_assignment/Pages/verify_otp.dart';
 
 class ForgotPassword extends StatefulWidget {
   const ForgotPassword({super.key});
@@ -12,20 +18,9 @@ class ForgotPassword extends StatefulWidget {
 class _ForgotPasswordState extends State<ForgotPassword> {
   final _formKey = GlobalKey<FormState>();
 
- String phoneNumber = "";
+  var phoneNumber = " ";
+
   final phoneNumberController = TextEditingController();
-  final otpController = TextEditingController();
-
-  bool visible = false;
-  var temp;
-  String otp = " "; 
-
-   @override
-  void dispose() {
-    phoneNumberController.dispose();
-    otpController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,9 +48,10 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                   TextButton(
                     onPressed: () {
                       Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => const Login()),
-                      );
+                                 context,
+                                 MaterialPageRoute(
+                                     builder: (context) => const Login()),
+                               );
                     },
                     child: const Text(
                       "Login",
@@ -68,19 +64,56 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                 ],
               ),
             ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  inputTextField("Contact Number", phoneNumberController, context),
-                  visible
-                      ? inputTextField("OTP", otpController, context)
-                      : const SizedBox(),
-                  !visible
-                      ? SendOTPButton("Send OTP")
-                      : SubmitOTPButton("Submit"),
-                ],
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.call),
+                  labelText: "Phone Number",
+                  hintText: "Enter the phone number",
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please Enter PhoneNumber";
+                  } else if (value.length != 10) {
+                    return "Enter Valid phoneNumber";
+                  }
+                  return null;
+                },
+                controller: phoneNumberController,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              child: ElevatedButton(
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(
+                    Color.fromARGB(255, 201, 94, 61),
+                  ),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    setState(
+                      () {
+                        phoneNumber = phoneNumberController.text;
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const VerifyOtp()),
+                        );
+                      },
+                    );
+                  }
+                },
+                child: const Text(
+                  "Send Code",
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
               ),
             ),
           ],
@@ -88,71 +121,5 @@ class _ForgotPasswordState extends State<ForgotPassword> {
       ),
     );
   }
-
-  Widget SendOTPButton(String text) => ElevatedButton(
-        onPressed: () async {
-          setState(() {
-            visible = !visible;
-          });
-          temp = await FirebaseAuthentication().sendOTP(phoneNumberController.text);
-        },
-        child: Text(text),
-      );
-
-  Widget SubmitOTPButton(String text) => ElevatedButton(
-        onPressed: () => FirebaseAuthentication().authenticate(temp, otpController.text),
-        child: Text(text),
-      );
-
-  Widget inputTextField(String labelText,
-          TextEditingController textEditingController, BuildContext context) =>
-      Padding(
-        padding:const EdgeInsets.all(10.00),
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width / 1.5,
-          child: TextFormField(
-            obscureText: labelText == "OTP" ? true : false,
-            controller: textEditingController,
-            decoration: InputDecoration(
-              hintText: labelText,
-              hintStyle:const TextStyle(color: Colors.blue),
-              filled: true,
-              fillColor: Colors.blue[100],
-              enabledBorder: OutlineInputBorder(
-                borderSide:const BorderSide(color: Colors.transparent),
-                borderRadius: BorderRadius.circular(5.5),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(color: Colors.transparent),
-                borderRadius: BorderRadius.circular(5.5),
-              ),
-            ),
-          ),
-        ),
-      );
 }
 
-class FirebaseAuthentication { 
-  String phoneNumber = ""; 
-  
-  sendOTP(String phoneNumber) async { 
-    this.phoneNumber = phoneNumber; 
-    FirebaseAuth auth = FirebaseAuth.instance; 
-    ConfirmationResult result = await auth.signInWithPhoneNumber( 
-      '+91$phoneNumber', 
-    ); 
-    printMessage("OTP Sent to +91 $phoneNumber"); 
-    return result; 
-  } 
-  
-  authenticate(ConfirmationResult confirmationResult, String otp) async { 
-    UserCredential userCredential = await confirmationResult.confirm(otp); 
-    userCredential.additionalUserInfo!.isNewUser 
-        ? printMessage("Authentication Successful") 
-        : printMessage("User already exists"); 
-  } 
-  
-  printMessage(String msg) { 
-    debugPrint(msg); 
-  } 
-}
